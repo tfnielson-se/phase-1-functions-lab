c5f92074e76a5c9318b25db063f5be03e3e5fe46
Thomas <tfnielson.se@gmail.com>
2022-12-12 21:44:20 -0700

    commit tree c3a4d54ff109d5128924cad89274f5a3c64c9be4

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Code your solution in this file!
+const loc = 42;
+let pickUp;
+let steps = 264;
+
+function distanceFromHqInBlocks (pickUp) {
+    let blocks = Math.abs(pickUp - loc) ;
+    return blocks
+}
+
+distanceFromHqInBlocks(43);
+distanceFromHqInBlocks(50);
+distanceFromHqInBlocks(34);
+
+function distanceFromHqInFeet (pickUp) {
+    let feet = Math.abs(pickUp - loc) * steps;
+    return feet;
+}
+
+distanceFromHqInFeet(43);
+distanceFromHqInFeet(50);
+distanceFromHqInFeet(34);
+
+function distanceTravelledInFeet (loc1, loc2) {
+    let travelDist = Math.abs(loc1 - loc2) * steps;
+    return travelDist;
+}
+
+distanceTravelledInFeet(43, 48);
+distanceTravelledInFeet(50, 60);
+distanceTravelledInFeet(34, 28);
+
+// pay = (start - dest) * steps - 400
+
+function calculatesFarePrice (start, destination) {
+    let calc = ((start - destination) * steps);
+    let distTraveled = Math.abs(calc - 400);
+    let midRange = distTraveled * 0.02
+
+    if (distTraveled <= 400) {
+        return 0
+    } else if (distTraveled > 401 && distTraveled <= 2000) {
+        return midRange;
+    } else if (distTraveled > 2001 && distTraveled < 2500) {
+        return 25
+    } else {
+        return `cannot travel that far`;
+    }
+    
+}
+
+calculatesFarePrice(43, 44);
+calculatesFarePrice(34, 32);
+calculatesFarePrice(50, 58);
+calculatesFarePrice(34, 24);
